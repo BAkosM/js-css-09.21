107f4f199d97f72c65f407e6e877506855dadcfc
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-21 12:52:56 +0200

    Button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import'bootstrap/dist/js/bootstrap.bundle.js';
-
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
 
 console.log('Loaded');
+
+let szinek = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'cyan',
+    'blue',
+    'purple',
+];
+function szinvaltas(){
+    console.log('katt');
+}
+function init() {
+document.getElementById('szinvalto').addEventListener('click',szinvaltas);
+}
+document.addEventListener('DOMContentLoaded',init);
